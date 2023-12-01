594af283eb84dfca5180eeb3ee8e07a08cbf1c76
Michael <michaelj.silva@hotmail.com>
2023-12-01 19:06:55 +0000

    Added starter code

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+var questions = [
+    {
+      question: "Which function is used to print something to the console in JavaScript?",
+      options: ["console.print()", "log.print()", "print.console()", "console.log()"],
+      answer: "console.log()"
+    },
+    {
+      question: "What does the '=== ' operator do in JavaScript?",
+      options: ["Checks equality without type coercion", "Assigns a value to a variable", "Performs a strict comparison", "Concatenates two strings"],
+      answer: "Performs a strict comparison"
+    },
+  ];
+  
